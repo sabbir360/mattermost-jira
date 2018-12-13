3b34e06581d4d7cbd41793a1cf3a741e4417d989
Sabbir Ahmed <sabbir1cse@outlook.com>
2018-12-13 17:46:15 +0600

    test code update

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from app import post_to_mattermost
 
-post_to_mattermost("Hello world.", icon="https://projects.leftofthedot.com:8443/secure/useravatar?ownerId=rezwanur&avatarId=12300")
+post_to_mattermost("Hello world.", icon="https://kbob.github.io/images/sample-4_thumb.jpg")
